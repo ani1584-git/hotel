7f0a292d7aef3554dcf2bb4fcba1863a3e93f65c
Aniruddh <ani@gmail.com>
2025-05-29 22:19:44 +0530

    version 3

--- connection.js
+++ connection.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-const url='mongodb://127.0.0.1:27017/hoteldb'
-mongoose.connect(url)
+//const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL;
+mongoose.connect(url,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 console.log("mongodb database connected successfully");
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import './connection.js'
 import mongoose from 'mongoose'
-
+import dotenv from 'dotenv'
 
 
 const app=express()
@@ -11,9 +11,12 @@ import  menuitemRoutes from './routes/menuitemRoutes.js'
 
 app.use(express.json());
 
+const PORT=process.env.PORT||3001
 
 app.use('/person',personRoutes)
 app.use('/menuitem',menuitemRoutes)
+
+
 
 app.listen(3001,()=>{
     console.log("server invoked at http://localhost:3001")
