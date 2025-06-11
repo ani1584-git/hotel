d213f09341ba68cc28bd85e5d722cf12f70bbd63
Aniruddh <ani@gmail.com>
2025-06-11 11:39:54 +0530

    version 5

--- server.js
+++ server.js
@@ -4,22 +4,37 @@ console.log("🚀 Server is starting...");
 import dotenv from 'dotenv'
 import express from 'express'
 import './connection.js'
-import mongoose from 'mongoose'
+import passport from './auth.js'
 
 dotenv.config();
 
 const app=express()
+
+const PORT=process.env.PORT||3001
+
+const logRequest=(req,res,next)=>{
+   console.log(`[${new Date().toLocaleString()}] request made to : ${req.originalUrl}`)
+   next()
+}
+
+app.use(logRequest)
+
+app.use(passport.initialize())
+
+const localAuthMiddleware=passport.authenticate('local',{session:false})
+
+app.get('/',function(req,res){
+  res.send('Welcome to hotel')
+})
 
 import personRoutes from './routes/personRoutes.js'
 import  menuitemRoutes from './routes/menuitemRoutes.js'
 
 app.use(express.json());
 
-const PORT=process.env.PORT||3001
 
-app.use('/person',personRoutes)
+app.use('/person',localAuthMiddleware,personRoutes)
 app.use('/menuitem',menuitemRoutes)
-
 
 
 app.listen(PORT, () => {
